1ce3c0e40072bf02de95346934bbe42ed5593eb2
Ambrus Simon <ambrussimon@invenshure.com>
2019-04-12 14:18:07 +0200

    Use SDK GearContext, populate manifest

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,18 @@
-FROM python:3.7.2-alpine3.9
+FROM python:3.7.3-alpine3.9
 MAINTAINER Flywheel <support@flywheel.io>
 ENV FLYWHEEL /flywheel/v0
 WORKDIR ${FLYWHEEL}
+
+RUN apk add --no-cache bash zip
+
+ENV BRU2NII_VERSION v1.0.20180303
 RUN set -eux \
-    && apk add --no-cache \
-        bash \
-        zip \
-    && wget https://github.com/neurolabusc/Bru2Nii/releases/download/v1.0.20180303/Bru2_Linux.zip \
+    && wget https://github.com/neurolabusc/Bru2Nii/releases/download/${BRU2NII_VERSION}/Bru2_Linux.zip \
     && unzip -d /bin Bru2_Linux.zip \
-    && rm Bru2_Linux.zip \
-    && mkdir -p ${FLYWHEEL}/input ${FLYWHEEL}/output
-COPY manifest.json manifest.json
-COPY run run
-ENTRYPOINT ["/flywheel/v0/run"]
+    && rm Bru2_Linux.zip
+
+ENV SDK_VERSION 6.1.0-dev.3
+RUN pip install flywheel-sdk==${SDK_VERSION}
+
+COPY manifest.json run.py ${FLYWHEEL}/
+ENTRYPOINT ["bash"]
---
+++ run.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import logging
+import os
+import re
+import subprocess
+import sys
+import zipfile
+
+import flywheel
+
+
+log = logging.getLogger('flywheel:Bru2Nii')
+
+
+if __name__ == '__main__':
+    with flywheel.GearContext() as context:
+        context.init_logging()
+        context.log_config()
+
+        input_filepath = context.get_input_path('bruker_file')
+        input_filename = os.path.basename(input_filepath)
+        with zipfile.ZipFile(input_filepath) as zf:
+            zf.extractall(context.work_dir)
+
+        cmd = [
+            'Bru2', # CLI version of Bru2Nii
+            '-v',   # verbose conversion comments
+            '-f',   # force conversion of localizer images (multiple slice orientations)
+            '-z',   # gz compress images (".nii.gz")
+        ]
+        if context.config.get('actual_size'):
+            cmd.append('-a')  # actual size (otherwise x10 scale so animals match humans)
+        output_filename = re.sub(r'\.pv\d\.zip$', '', input_filename)
+        output_filepath = os.path.join(context.output_dir, output_filename)
+        cmd.extend([
+            '-o', output_filepath,  # output filename(s) based on input filename (sans ext)
+            context.work_dir,       # extracted bruker files (Bru2 finds acqp recursively)
+        ])
+
+        log.info('calling Bru2: %s', cmd)
+        subprocess.check_call(cmd)
+
+        nifti_outputs = os.listdir(context.output_dir)
+        if not nifti_outputs:
+            log.error('No NIfTI outputs created')
+            sys.exit(1)
+
+        for nifti in nifti_outputs:
+            context.update_file_metadata(nifti, type='nifti', modality='MR')
